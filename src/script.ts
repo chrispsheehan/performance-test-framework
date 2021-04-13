93eacb3ac06749ff120bcadaa7df7fc4790fa252
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-04-13 16:55:50 +0100

    basic checks

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,23 @@
 import { check } from "k6";
 import http from "k6/http";
 
+
+export let options = {
+
+  thresholds: {
+
+    http_req_failed: ['rate<0.01'],   // http errors should be less than 1% 
+
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+
+  },
+
+};
+
 export default function() {
+
   let res = http.get("http://localhost:49160/");
+
   check(res, {
     "is status 200": (r) => r.status === 200
   });
