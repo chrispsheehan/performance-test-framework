93c6382bc431d6f491efa7f4b51d0f3fc205d312
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-04-13 16:20:53 +0100

    pointed at test app

--- src/script.ts
+++ src/script.ts
@@ -2,7 +2,7 @@ import { check } from "k6";
 import http from "k6/http";
 
 export default function() {
-  let res = http.get("https://test.loadimpact.com/");
+  let res = http.get("http://localhost:49160/");
   check(res, {
     "is status 200": (r) => r.status === 200
   });
