c5e1f349362e2715d2ebdf4c8074e8deb8a7dbcf
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-04-16 13:31:23 +0100

    logging

--- TestApp/src/server.js
+++ TestApp/src/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
 const app = express();
+const port = 3000
 
 app.get('/', (req, res) => {
-    console.log('yo')
+
+    console.log('hit OK enpoint');
     res.status(200).send('hello world :D');
 });
 
 app.get('/slow/', (req, res) => {
     
+    console.log('hit slow endpoint');
     setTimeout(function() {
         res.json(["badgers"]);
     }, 5000);    
@@ -16,5 +19,10 @@ app.get('/slow/', (req, res) => {
 
 app.get('/error/', (req, res) => {
     
+    console.log('hit error endpoint');
     throw new Error('blah errors'); 
 });
+
+app.listen(port, () => {
+  console.log(`running on http://localhost:${port}/`)  
+})
